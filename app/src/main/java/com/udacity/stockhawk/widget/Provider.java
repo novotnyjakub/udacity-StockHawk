4787b6c90d51ba16e80d2333e876f509818c8edd
Jakub Novotny <novotny.jakub@gmail.com>
2017-02-20 22:48:04 +0000

    Add blank widget with config

---
+++ app/src/main/java/com/udacity/stockhawk/widget/Provider.java
@@ -0,0 +1,20 @@
+package com.udacity.stockhawk.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+public class Provider extends AppWidgetProvider {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
